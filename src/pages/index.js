8eb2c4e132e48ff86416b4129ecfd86ea8ce6fe9
zobry <apirat.chiwa@gmail.com>
2021-01-02 21:32:59 +0700

    create data to db firestore

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
+import { db, firebase } from 'services/firebase';
+
+const photosRef = db.collection('photos')
+
+photosRef.add({
+    title: 'Lens',
+    owner: 'Markus Spiske',
+    url: 'https://source.unsplash.com/zssAC1KCzNs',
+    created_at: firebase.firestore.Timestamp.fromDate(new Date())
+})
+
 function Home() {
     return <div>
         <h1>Home Page</h1>
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
+import 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "",
@@ -15,4 +16,9 @@ if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
 
-export { firebase }
+const db = firebase.firestore()
+
+export { 
+    firebase, 
+    db 
+}
