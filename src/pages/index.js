15e956fdedec6995a8cc4943402d51e4d44ccbdc
zobry <apirat.chiwa@gmail.com>
2021-01-04 12:02:29 +0700

    handle show/hide data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useRef, useEffect, useReducer } from 'react';
 import { db } from 'services/firebase';
 
 // const photosRef = db.collection('photos')
@@ -10,38 +10,89 @@ import { db } from 'services/firebase';
 //     created_at: firebase.firestore.Timestamp.fromDate(new Date())
 // })
 
-function Home() {
-    const [snapshot, setSnapshot] = useState(null)
+function reducer(state, action) {
+    switch (action.type) {
+        case 'value':
+            return {
+                ...state,
+                value: action.value,
+                loading: false,
+                error: undefined
+            }
+        case 'error':
+            return {
+                ...state,
+                value: undefined,
+                loading: false,
+                error: action.error
+            }
+        default:
+            return state
+    }
+}
+
+function getInitialState(initValue) {
+    return {
+        loading: initValue === null,
+        value: initValue
+    }
+}
+
+function useCollection(query) {
+    const ref = useRef(query)
+    const [state, dispatch] = useReducer(reducer, getInitialState())
 
     useEffect(() => {
-        db.collection('photos').orderBy('created_at').limit(16)
-        .get()
-        .then( snapshot => {
-            setSnapshot(snapshot)
-        })
-        .catch(
-            err => console.log(err)
+        if (ref.current === null) {
+            dispatch({ type: 'value', value: null })
+            return
+        }
+        ref.current.get()
+        .then( snapshot => 
+            dispatch({ type: 'value', value: snapshot })
+        )
+        .catch( err => 
+            dispatch({ type: 'error', error: err }) 
         )
     }, [])
 
-    if (snapshot === null) {
+    return {
+        data: state.value 
+            ? state.value.docs.map( e => {
+                return {
+                    id: e.id,
+                    ...e.data(),
+                }
+            })
+            : null,
+        loading: state.loading,
+        error: state.error,
+        empty: state.value ? state.value.empty : null
+    }
+}
+
+function Home() {
+    const { loading, error, data, empty } = useCollection(
+        db.collection('photos').orderBy('created_at').limit(16)
+    )
+
+    if (loading) {
         return <div>Loading..</div>
     }
-
-    const data = snapshot.docs.map( e => {
-        return {
-            id: e.id,
-            ...e.data(),
-        }
-    })
+    if (empty) {
+        return <div>Nothing to show..</div>
+    }
+    if (error) {
+        return <div>Error..</div>
+    }
 
     return <div>
         <h1>Home Page</h1>
-        {data.map(e => {
+        {data && data.map(e => {
             return (
                 <div key={e.id}>
                     <img src={e.url} alt={e.title} />
-                    {e.title}
+                    <h3>{e.title}</h3>
                 </div>
             )
         })}
